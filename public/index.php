56705aa8ced85a53f9efb42d31b08e6555518ff6
Andrew Joseph <andrew@aljweb.com>
2014-07-14 19:21:45 -0500

    cleanup

--- public/index.php
+++ public/index.php
@@ -6,52 +6,66 @@ if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
     // dependencies were installed via composer - this is the main project
     $classLoader = require __DIR__ . '/../vendor/autoload.php';
 } else {
-    throw new \Exception('Can\'t find autoload.php. Did you install dependencies via composer?');
+    throw new \Exception('Can\'t find autoload.php.');
 }
 
-use War\Deck;
 use War\Game;
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
     <title>War</title>
 </head>
 <body>
+<div>
+    <comment>
+        Personal note: This is only written in PHP because the original
+        requirements for the project stipulated use of PHP, for proof of
+        concept.  In the real world this should be written in something
+        like C++ obviously :)
+    </comment>
 <pre>
 /**
     Code Challenges:
 
     WAR:
-    Design a class definition for a deck of playing cards, then write a simple program to simulate two
-    players playing the game of War. The game and rules are described here: <a href="http://www.pagat.com/war/war.html">http://www.pagat.com/war/war.html</a>
+    Design a class definition for a deck of playing cards, then write a
+    simple program to simulate two players playing the game of War. The
+    game and rules are described here:
+    <a href="http://www.pagat.com/war/war.html">http://www.pagat.com/war/war.html</a>
 
     The program should:
     * Shuffle the virtual deck before every game
     * Deal 26 cards to each player
-    * Display the cards that were played for each turn, who was the winner, and the running score.
+    * Display the cards that were played for each turn, who was the
+      winner, and the running score.
 
  */
 </pre>
 
+<div>
     <h1>Codexorz</h1>
-    <a href="index.php.html">index.php</a><br />
-    <a href="Deck.php.html">Deck.php</a><br />
-    <a href="War.php.html">War.php</a><br />
+    <p>
+        <a href="https://github.com/ofus/war2">https://github.com/ofus/war2</a>
+    </p>
+</div>
 
     <h1>Output</h1>
-<pre>
-<?php
+    <pre>
+    <?php
 
-$game = new War\Game();
-while ( $game->doRound() ) {
-    echo $game->getLog();
-}
-echo "Winner: " . $game->getWinner() . PHP_EOL;
-echo "Game over on turn #" . $game->getTurn() . PHP_EOL;
+    $game = new Game();
+    echo "Begin!\n";
+    for(;;) {
+        $game->doRound();
+        echo $game->getLog();
+    }
+    echo "Winner: " . $game->getWinner() . PHP_EOL;
+    echo "Game over on turn #" . $game->getTurn() . PHP_EOL;
 
-?>
-</pre>
+    ?>
+    </pre>
+</div>
 </body>
 </html>
